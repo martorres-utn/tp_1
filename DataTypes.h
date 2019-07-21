6069dc8a18d5d07e74550e5ca7c208f5ac1ed384
martorres-utn <51000566+martorres-utn@users.noreply.github.com>
2019-07-21 13:18:12 -0300

    Se creó el tipo de datos para manejar pokemons. Está en el encabezado DataTypes.

---
+++ DataTypes.h
@@ -0,0 +1,15 @@
+//
+// Created by Marcos on 7/21/2019.
+//
+
+#ifndef TP_1_DATATYPES_H
+#define TP_1_DATATYPES_H
+
+struct PokemonType
+{
+    char Type;
+    int Level;
+    char Name[10];
+};
+
+#endif //TP_1_DATATYPES_H
--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
+#include <bits/stdc++.h>
+#include "DataTypes.h"
 
 int main() {
+
+    PokemonType myPokemon;
+
+    myPokemon.Level = 1;
+    myPokemon.Type = 'F';
+
+    // like if you were assigning myPokemon.Name = "Juancho" because you can't assign string values directly to char array. You will have to copy them.
+    strcpy(myPokemon.Name, "Juancho");
 
     int number1 = 3;
     int number2 = 4;
     int number4 = 5;
     int result = number1 + number2 + number4;
     std::cout << "Hello, World! - result:" << result << std::endl;
+    std::cout << "pokemon.Level:" << myPokemon.Level << std::endl;
+    std::cout << "pokemon.Type:" << myPokemon.Type << std::endl;
+    std::cout << "pokemon.Name:" << myPokemon.Name << std::endl;
     return 0;
 }
