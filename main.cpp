3fdd44628a63ab9ed375af0bdd7a4b0df020db1d
martorres-utn <51000566+martorres-utn@users.noreply.github.com>
2019-07-21 23:03:10 -0300

    Update main.cpp
    Cree un menú muy básico para comenzar a separar el programa en funcionalidades. Por ahora : se puede ingresar pokemons, consultar cantidad de pokemons de cada tipo (función a desarrollar), Salir de la pokedex.

--- main.cpp
+++ main.cpp
@@ -3,22 +3,88 @@
 #include "DataTypes.h"
 
 int main() {
+    //main menu options
+    std::map<int, std::string> mapOptions = {
+            {1, "Ingresar Pokemons"},
+            {2, "Consultar Cantidad de Pokemons de cada tipo"},
+            {3, "Salir del la Pokedex"}
+    };
 
-    PokemonType myPokemon;
+    std::vector<PokemonType> pokemonVector;
 
-    myPokemon.Level = 1;
-    myPokemon.Type = 'F';
+    std::cout << "Bienvenido a la Pokedex!" << std::endl;
 
-    // like if you were assigning myPokemon.Name = "Juancho" because you can't assign string values directly to char array. You will have to copy them.
-    strcpy(myPokemon.Name, "Juancho");
+    //selectedOption: will contain user's selected option, this variable will be assigned from the console input.
+    int selectedOption = 0;
 
-    int number1 = 3;
-    int number2 = 4;
-    int number4 = 5;
-    int result = number1 + number2 + number4;
-    std::cout << "Hello, World! - result:" << result << std::endl;
-    std::cout << "pokemon.Level:" << myPokemon.Level << std::endl;
-    std::cout << "pokemon.Type:" << myPokemon.Type << std::endl;
-    std::cout << "pokemon.Name:" << myPokemon.Name << std::endl;
+    while(selectedOption != 3)
+    {
+        std::cout << "Selecciona una opcion." << std::endl;
+
+        //print available options using mapOptions
+        for(auto it = mapOptions.begin(); it != mapOptions.end(); ++it)
+        {
+            std::cout << (*it).second << "[" << (*it).first << "]" << std::endl;
+        }
+
+        //read console option
+        std::cout << "tu opcion:";
+        std::cin >> selectedOption;
+
+        switch(selectedOption)
+        {
+            case 1:
+            {
+                std::cout << "Ingresa Todos los Pokemons que quieras. Ingresa: TIPO, NIVEL, NOMBRE. Ingresa TIPO Q para terminar la lista de Pokemons y volver al menu..." << std::endl;
+                int inputIndex = 0;
+                PokemonType newPokemon;
+
+                do
+                {
+                    std::cout << "ingresa TIPO (T para tierra, F para fuego, A para agua, E para electrico):" << std::endl;
+                    std::cin >> newPokemon.Type;
+
+                    if(newPokemon.Type != 'Q')
+                    {
+                        std::cout << "ingresa NIVEL (valor numerico):" << std::endl;
+                        std::cin >> newPokemon.Level;
+
+                        std::cout << "ingresa NOMBRE (valor alfanumerico , 10 max):" << std::endl;
+                        std::cin >> newPokemon.Name;
+
+                        pokemonVector.push_back(newPokemon);
+
+                        inputIndex++;
+                    }
+
+                }while(newPokemon.Type != 'Q' && inputIndex < 100);
+
+                //printing just for debug
+                std::cout << "Pokemons ingresados:" << std::endl;
+                for(auto it = pokemonVector.begin(); it != pokemonVector.end() ; ++it)
+                {
+                    std::cout << (*it).Type << "-" << (*it).Level << "-" << (*it).Name << std::endl;
+                }
+
+                break;
+            }
+
+            case 2:
+            {
+                break;
+            }
+
+            case 3:
+            {
+                break;
+            }
+
+            default:
+            {
+                break;
+            }
+        }
+    }
+
     return 0;
 }
