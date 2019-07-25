135db3425ed92d6930ef0649b998a2199b72386e
martorres-utn <51000566+martorres-utn@users.noreply.github.com>
2019-07-25 11:22:59 -0300

    Mudé la lógica del registro de pokemons a un procedimiento. Agregué el procedimiento que muestra la cantidad de pokemons por tipo ingresados.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,68 @@
 #include "DataTypes.h"
 #include "includes/TerminalUtilities.h"
 
+void registerNewPokemons(std::vector<PokemonType> & pokemonVector)
+{
+    std::cout << "Ingresa Todos los Pokemons que quieras.\n\tIngresa: TIPO, NIVEL, NOMBRE.\n\tIngresa TIPO Q para terminar la lista de Pokemons y volver al menu..." << std::endl;
+    int inputIndex = pokemonVector.size();
+    PokemonType newPokemon;
+
+    do
+    {
+
+        //solicita valor char por terminal, la expresion sera evaluada por regular expresion para coincidir con: T, F, A, E, Q(termina el ingreso)
+        TerminalUtilities::getValidValue<char>(newPokemon.Type, "Ingresa TIPO (T para Tierra, F para Fuego, A para Agua, E para Electrico, Q para terminar el ingreso):>", "^(T|F|A|E|Q)$",  "\tERROR! (ingresa un tipo valido: T,F,A,E)");
+
+        if(newPokemon.Type != 'Q')
+        {
+            //solicita valor int por terminal, la expresion sera evaluada por regular expression para coincidir con un numero entre 1 y 1000.
+            TerminalUtilities::getValidValue<int>(newPokemon.Level, "Ingresa NIVEL (valor entero entre 1 y 1000):>", "^([1-9][0-9]{0,2}$|^1000)$", "\tERROR! (ingresa un valor entero entre 1 y 1000)");
+
+            //solicita valor char[10] por terminal, la expresion sera evaluada por regular expression para coincidir con una combinacion de letras mayusculas y minusculas entre a y z sin considerar caracteres especiales (longitud entre 1 y 10)
+            TerminalUtilities::getValidValue<char[10]>(newPokemon.Name, "Ingresa NOMBRE (cadena de letras, 10 max):>", "^[a-zA-Z]{1,10}$", "\tERROR! (ingresa una cadena de letras, 10 longitud max)");
+
+            pokemonVector.push_back(newPokemon);
+
+            inputIndex++;
+
+            std::cout << "----------------------[ #" << inputIndex << " Pokemon registrado ]--------------------------------" << std::endl;
+        }
+
+    }while(newPokemon.Type != 'Q');
+
+    std::cout << "------------------------------------------------------" << std::endl;
+
+    //printing just for debug
+    std::cout << "Pokemons ingresados:" << std::endl;
+    for(auto it = pokemonVector.begin(); it != pokemonVector.end() ; ++it)
+    {
+        std::cout << "\t - TIPO: " << (*it).Type << " , NIVEL: " << (*it).Level << " , NOMBRE: " << (*it).Name << std::endl;
+    }
+}
+
+void showAmountOfPokemonsByType(std::vector<PokemonType> & pokemonVector)
+{
+    std::map<char, int> typeAmount = {};
+
+    for(auto it = pokemonVector.begin(); it != pokemonVector.end(); ++it)
+    {
+        PokemonType pokemon = (*it);
+
+        if(typeAmount.find(pokemon.Type) == typeAmount.end())
+            typeAmount[pokemon.Type] = 1;
+        else
+            typeAmount[pokemon.Type]++;
+    }
+
+    std::cout << "Cantidad de Pokemons por tipo" << std::endl;
+    for(auto it = typeAmount.begin(); it != typeAmount.end(); ++it)
+    {
+        char type = (*it).first;
+        int amount = (*it).second;
+        std::cout << "\tTipo: " << type << " - Cantidad: " << amount << std::endl;
+    }
+}
+
 int main() {
     //main menu options
     std::map<int, std::string> mapOptions = {
@@ -31,52 +93,23 @@ int main() {
         //print available options using mapOptions
         for(auto it = mapOptions.begin(); it != mapOptions.end(); ++it)
         {
-            std::cout << "[" << (*it).first << "] - " << (*it).second << std::endl;
+            std::cout << "\t[" << (*it).first << "] - " << (*it).second << std::endl;
         }
 
         //solicita valor entero por terminal, la expresion sera evaluada por la regular expression del 3er parametro.
-        TerminalUtilities::getValidValue<int>(selectedOption, "Ingresa tu opcion:>", "^\\d{1}$", "ERROR! (ingresa un numero entero entre 1 y 9)");
+        TerminalUtilities::getValidValue<int>(selectedOption, "Ingresa tu opcion:>", "^\\d{1}$", "\tERROR! (ingresa un numero entero entre 1 y 9)");
 
         switch(selectedOption)
         {
             case 1: //REGISTRAR POKEMONS
             {
-                std::cout << "Ingresa Todos los Pokemons que quieras. Ingresa: TIPO, NIVEL, NOMBRE. Ingresa TIPO Q para terminar la lista de Pokemons y volver al menu..." << std::endl;
-                int inputIndex = 0;
-                PokemonType newPokemon;
-
-                do
-                {
-                    //solicita valor char por terminal, la expresion sera evaluada por regular expresion para coincidir con: T, F, A, E, Q(termina el ingreso)
-                    TerminalUtilities::getValidValue<char>(newPokemon.Type, "Ingresa TIPO (T para Tierra, F para Fuego, A para Agua, E para Electrico):>", "^(T|F|A|E|Q)$",  "ERROR! (ingresa un tipo valido: T,F,A,E)");
-
-                    if(newPokemon.Type != 'Q')
-                    {
-                        //solicita valor int por terminal, la expresion sera evaluada por regular expression para coincidir con un numero entre 1 y 100.
-                        TerminalUtilities::getValidValue<int>(newPokemon.Level, "Ingresa NIVEL (valor entero entre 1 y 100):>", "^[1-9][0-9]{1,2}$|^1000$", "ERROR! (ingresa un valor entero entre 1 y 1000)");
-
-                        //solicita valor char[10] por terminal, la expresion sera evaluada por regular expression para coincidir con una combinacion de letras mayusculas y minusculas entre a y z sin considerar caracteres especiales (longitud entre 1 y 10)
-                        TerminalUtilities::getValidValue<char[10]>(newPokemon.Name, "Ingresa NOMBRE (cadena de letras, 10 max):>", "^[a-zA-Z]{1,10}$", "ERROR! (ingresa una cadena de letras, 10 longitud max)");
-
-                        pokemonVector.push_back(newPokemon);
-
-                        inputIndex++;
-                    }
-
-                }while(newPokemon.Type != 'Q' && inputIndex < 100);
-
-                //printing just for debug
-                std::cout << "Pokemons ingresados:" << std::endl;
-                for(auto it = pokemonVector.begin(); it != pokemonVector.end() ; ++it)
-                {
-                    std::cout << "> TIPO: " << (*it).Type << " , NIVEL: " << (*it).Level << " , NOMBRE: " << (*it).Name << std::endl;
-                }
-
+                registerNewPokemons(pokemonVector);
                 break;
             }
 
             case 2: //CANTIDAD DE POKEMONS POR TIPO
             {
+                showAmountOfPokemonsByType(pokemonVector);
                 break;
             }
 
